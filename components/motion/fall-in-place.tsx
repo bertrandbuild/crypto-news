8a40fa5b8575c540dfcec744d59cd826353a3e89
stepome <stefanopomelli23@gmail.com>
2024-03-17 00:59:14 +0100

    home page

--- components/motion/fall-in-place.tsx
+++ components/motion/fall-in-place.tsx
@@ -1,17 +1,17 @@
-import { MotionBox, MotionBoxProps } from './box'
-import React from 'react'
+import { MotionBox, MotionBoxProps } from "./box";
+import React from "react";
 
 export const FallInPlace: React.FC<MotionBoxProps & { delay?: number }> = (
   props
 ) => {
-  const { children, delay = 0.2, ...rest } = props
+  const { children, delay = 0.2, ...rest } = props;
   return (
     <MotionBox
-      initial={{ scale: 1, opacity: 0, translateY: '20px' }}
+      initial={{ scale: 1, opacity: 0, translateY: "20px" }}
       animate={{ scale: 1, opacity: 1, translateY: 0 }}
       transition={{
-        type: 'tween',
-        ease: 'easeOut',
+        type: "tween",
+        ease: "easeOut",
         duration: 2,
         delay,
       }}
@@ -19,5 +19,5 @@ export const FallInPlace: React.FC<MotionBoxProps & { delay?: number }> = (
     >
       {children}
     </MotionBox>
-  )
-}
+  );
+};
