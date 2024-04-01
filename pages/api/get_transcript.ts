077d8763947e639c87980f1b63de33aa6836ca2b
Bertrand <bertrandrigal.pro@gmail.com>
2024-04-01 15:43:49 +0200

    wip : delete useless logic + move the galadriel logic in specific file

--- pages/api/get_transcript.ts
+++ pages/api/get_transcript.ts
@@ -1,36 +1,36 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { getYtTranscript } from '../../utils/callApi'; // Update the path as needed
-import * as testTranscript from './data.json';
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from 'next';
+// import { getYtTranscript } from '../../utils/callApi'; // Update the path as needed
+// import * as testTranscript from './data.json';
 
-function cleanupTranscript(transcript) {
-  // Use a regular expression to remove the "Time: [timestamp] - Text: " part from each line
-  const cleanedTranscript = transcript.replace(/Time: \d+\.\d+ - Text: /g, '').replace(/\n/g, ' ');
-  return cleanedTranscript;
-}
+// function cleanupTranscript(transcript) {
+//   // Use a regular expression to remove the "Time: [timestamp] - Text: " part from each line
+//   const cleanedTranscript = transcript.replace(/Time: \d+\.\d+ - Text: /g, '').replace(/\n/g, ' ');
+//   return cleanedTranscript;
+// }
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<any>
-) {
-  if (req.method === 'GET') {
-    const videoId = req.query.videoId as string;
-    if (!videoId) {
-      return res.status(400).json({ error: 'Video ID is required' });
-    }
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<any>
+// ) {
+//   if (req.method === 'GET') {
+//     const videoId = req.query.videoId as string;
+//     if (!videoId) {
+//       return res.status(400).json({ error: 'Video ID is required' });
+//     }
 
-    // const isDev = process.env.NODE_ENV === 'development';
-    const isDev = false;
-    try {
-      const transcript = (isDev) ? testTranscript.transcript : await getYtTranscript(videoId);
-      const cleanedTranscript = cleanupTranscript(transcript);
-      res.status(200).json(cleanedTranscript);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  } else {
-    // Handle any other HTTP method
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-}
+//     // const isDev = process.env.NODE_ENV === 'development';
+//     const isDev = false;
+//     try {
+//       const transcript = (isDev) ? testTranscript.transcript : await getYtTranscript(videoId);
+//       const cleanedTranscript = cleanupTranscript(transcript);
+//       res.status(200).json(cleanedTranscript);
+//     } catch (error) {
+//       res.status(500).json({ error: error.message });
+//     }
+//   } else {
+//     // Handle any other HTTP method
+//     res.setHeader('Allow', ['GET']);
+//     res.status(405).end(`Method ${req.method} Not Allowed`);
+//   }
+// }
--- utils/callApi.ts
+++ utils/callApi.ts
@@ -3,51 +3,53 @@ import fs from 'fs';
 
 const { exec } = require('child_process');
 
-function getYtTranscript(videoId: string): Promise<string> {
-  return new Promise((resolve, reject) => {
-    exec(`python3 scripts/get_transcript.py ${videoId}`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(error);
-        reject(new Error(`Execution error: ${error.message}`));
-        return;
-      }
-      if (stderr) {
-        console.error(stderr);
-        reject(new Error(`Script error: ${stderr}`));
-        return;
-      }
-      resolve(stdout.trim());
-    });
-  });
-}
+// function getYtTranscript(videoId: string): Promise<string> {
+//   return new Promise((resolve, reject) => {
+//     exec(`python3 scripts/get_transcript.py ${videoId}`, (error, stdout, stderr) => {
+//       if (error) {
+//         console.error(error);
+//         reject(new Error(`Execution error: ${error.message}`));
+//         return;
+//       }
+//       if (stderr) {
+//         console.error(stderr);
+//         reject(new Error(`Script error: ${stderr}`));
+//         return;
+//       }
+//       resolve(stdout.trim());
+//     });
+//   });
+// }
 
-function getSummarizedTranscript(transcript: string): Promise<string> {
-  // shorten the transcript to about 10,000 characters to avoid hitting the max gas limit
-  transcript = transcript.slice(0, 11000); // TODO: handle longer transcripts
-  const filePath = path.join(process.cwd(), './contracts/scripts/input.txt');
-  console.log(transcript.length);
-  fs.writeFile(filePath, transcript, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  });
+// function getSummarizedTranscript(transcript: string): Promise<string> {
+//   // shorten the transcript to about 10,000 characters to avoid hitting the max gas limit
+//   transcript = transcript.slice(0, 11000); // TODO: handle longer transcripts
+//   const filePath = path.join(process.cwd(), './contracts/scripts/input.txt');
+//   console.log(transcript.length);
+//   fs.writeFile(filePath, transcript, (err) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//   });
 
-  return new Promise((resolve, reject) => {
-    exec(`cd  ./contracts && npm run gpt-text`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(error);
-        reject(new Error(`Execution error: ${error.message}`));
-        return;
-      }
-      if (stderr) {
-        console.error(stderr);
-        reject(new Error(`Script error: ${stderr}`));
-        return;
-      }
-      resolve(stdout.trim());
-    });
-  });
-}
+//   return new Promise((resolve, reject) => {
+//     exec(`cd  ./contracts && npm run gpt-text`, (error, stdout, stderr) => {
+//       if (error) {
+//         console.error(error);
+//         reject(new Error(`Execution error: ${error.message}`));
+//         return;
+//       }
+//       if (stderr) {
+//         console.error(stderr);
+//         reject(new Error(`Script error: ${stderr}`));
+//         return;
+//       }
+//       resolve(stdout.trim());
+//     });
+//   });
+// }
 
-export { getYtTranscript, getSummarizedTranscript };
+export { getYtTranscript, 
+  // getSummarizedTranscript 
+};
