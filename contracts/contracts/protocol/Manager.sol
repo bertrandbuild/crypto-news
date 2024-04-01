89d81c29abe981876412ca1bec14a58451495926
Claudia <claudiabarcelovaldes40@gmail.com>
2024-04-01 19:06:19 +0200

    fix: set function as payable

--- contracts/contracts/protocol/Manager.sol
+++ contracts/contracts/protocol/Manager.sol
@@ -18,7 +18,7 @@ contract Manager is Users, Ownable {
         emit CreditsPriceUpdated(_creditsPrice);
     }
 
-    function fundUser(uint256 _newCredits) public {
+    function fundUser(uint256 _newCredits) public payable {
         uint256 calculatedValue = _calcValue(_newCredits);
         if (msg.value != calculatedValue) revert InvalidValue(msg.value, calculatedValue);
         _fundUser(_newCredits);
