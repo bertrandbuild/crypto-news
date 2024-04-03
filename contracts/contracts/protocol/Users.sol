7b3973ee900689adc0989ec4a05578dd52439f22
Juan Fuente <127140423+Juan-Fuente-T@users.noreply.github.com>
2024-04-03 18:47:55 +0200

    Fix:Ajustando detalles

--- contracts/contracts/protocol/Users.sol
+++ contracts/contracts/protocol/Users.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
+pragma solidity ^0.8.8;
 
 contract Users {
     // mapping fo user credits
@@ -23,7 +23,7 @@ contract Users {
      */
     function _useCredit(address _userId) internal {
         credits[_userId] -= 1;
-        CreditUsed(_userId); // event might not be needed (adding it to follow convention)
+        emit CreditUsed(_userId); // event might not be needed (adding it to follow convention)
     }
 
     /**
--- contracts/test/FileStorage.ts
+++ contracts/test/FileStorage.ts
@@ -1,12 +1,22 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+import {Signer, BigNumberish } from "ethers";
+
 
 describe("FileStorage", function () {
-  let fileStorage, owner, user1, user2, fee;
+  let fileStorage: any;
+  let owner: Signer;
+  let user1: Signer;
+  let user2: Signer;
+  let fee: BigNumberish;
 
   // Deploy the contract
   before(async function () {
-    const fileStorage = await ethers.deployContract('FileStorage', [ethers.parseEther("0.0001")]);
+    // AQUÍ ESTÁ EL PROBLEMA:
+    //const fileStorage = await ethers.deployContract('FileStorage', [ethers.parseEther("0.0001")]);
+    // Aquí debes asignar el contrato desplegado a la variable fileStorage, no declarar una nueva variable
+    fileStorage = await ethers.deployContract('FileStorage', [ethers.parseEther("0.0001")]);
+   
     await fileStorage.waitForDeployment();
 
     // Get signers
@@ -21,7 +31,7 @@ describe("FileStorage", function () {
     // User1 adds a file
     await expect(fileStorage.connect(user1).addFile(fileId, transcriptCid, analysisCid))
       .to.emit(fileStorage, "FileAdded")
-      .withArgs(fileId, transcriptCid, analysisCid, user1.address);
+      .withArgs(fileId, transcriptCid, analysisCid, user1.getAddress());
   });
 
   it("Should allow another user to pay the fee and access the file info", async function () {
@@ -31,11 +41,45 @@ describe("FileStorage", function () {
     const transaction = await fileStorage.connect(user2).getFile(fileId, { value: fee });
     await expect(transaction)
       .to.emit(fileStorage, "FileInfoAccessed")
-      .withArgs(fileId, "transcriptCidExample", "analysisCidExample", user2.address);
+      .withArgs(fileId, "transcriptCidExample", "analysisCidExample", user2.getAddress());
+
+      //const fee: BigNumber = ethers.BigNumber.from(100); // Por ejemplo
+
+      
+      const feeNum = ethers.utils.parseEther("0.01");
+      
 
     // Verify the transfer to the first user
     await expect(() => transaction)
-      .to.changeEtherBalances([user2, user1], [fee.mul(-1), fee]);
+    .to.changeEtherBalances([user2, user1], [feeNum * -1, feeNum]);
+  });
+
+  it("Should create a table upon deployment", async function () {
+    const tableId = await fileStorage.getTableId();
+    expect(tableId).to.not.equal(0);
+  });
+
+  it("Should allow updating a file entry", async function () {
+    const fileId = 1;
+    const updatedTranscriptCid = "updatedTranscriptCid";
+    const updatedAnalysisCid = "updatedAnalysisCid";
+  
+    // Update the file entry
+    await expect(fileStorage.updateTable(fileId, updatedTranscriptCid, updatedAnalysisCid))
+      .withArgs(fileId, updatedTranscriptCid, updatedAnalysisCid);
+  });
+
+  it("Should allow deleting a file entry", async function () {
+    const fileId = 1;
+  
+    // Delete the file entry
+    await expect(fileStorage.deleteFromTable(fileId))
+      .withArgs(fileId);
+  
+    // Try to access the deleted entry and verify it's deleted
+    const fileInfo = await fileStorage.fileInfoMap(fileId);
+    expect(fileInfo.transcriptCid).to.equal("");
+    expect(fileInfo.analysisCid).to.equal("");
   });
 
 });
